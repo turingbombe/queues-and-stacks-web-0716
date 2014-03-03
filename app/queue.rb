f13df287c795f06c2df70baceb39f28ee865670b
Scott C. Reynolds <scott@scottcreynolds.com>
2014-03-03 12:03:30 -0500

    solutions

--- app/queue.rb
+++ app/queue.rb
@@ -2,4 +2,22 @@ require_relative 'stack'
 
 class Queue
 
+  def initialize
+    @in_stack = Stack.new
+    @out_stack = Stack.new
+  end
+
+  def enqueue(val)
+    @in_stack.push val
+  end
+
+  def dequeue
+    if @out_stack.size == 0
+      while @in_stack.size > 0
+        @out_stack.push @in_stack.pop
+      end
+    end
+    @out_stack.pop
+  end
+
 end
--- app/stack.rb
+++ app/stack.rb
@@ -1,3 +1,20 @@
 class Stack
 
+  def initialize
+    @elements = []
+  end
+
+  def push(datum)
+    @elements.push datum
+    self
+  end
+
+  def pop
+    @elements.pop
+  end
+
+  def size
+    @elements.size
+  end
+
 end
