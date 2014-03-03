a285e19f1228911f3f784629e5614dffa056d873
Scott C. Reynolds <scott@scottcreynolds.com>
2014-03-03 11:57:14 -0500

    add size test for stack, kill solutions

--- app/queue.rb
+++ app/queue.rb
@@ -1,12 +1,5 @@
 require_relative 'stack'
 
 class Queue
-  def initialize
-    @instack = Stack.new
-    @outstack = Stack.new
-  end
 
-  def enqueue(val)
-    @instack.push val
-  end
 end
--- app/stack.rb
+++ app/stack.rb
@@ -1,15 +1,3 @@
 class Stack
 
-  def initialize
-    @elements = []
-  end
-
-  def push(datum)
-    @elements.push datum
-    self
-  end
-
-  def pop
-    @elements.pop
-  end
 end
