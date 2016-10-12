9fe9c7a4b2ee7fb43a8f72801874e969751a0a0b
Charlie Cook <turingbombe@gmail.com>
2016-10-12 14:39:11 -0400

    completed queues and stacks lab

--- lib/queue.rb
+++ lib/queue.rb
@@ -1,5 +1,16 @@
 require_relative 'stack'
 
 class Queue
+	attr_accessor :queue
+	def initialize
+		@queue = Stack.new
+	end
 
+	def enqueue(var)
+		@queue.push(var)
+	end
+
+	def dequeue
+		@queue.stack.shift
+	end
 end
--- lib/stack.rb
+++ lib/stack.rb
@@ -1,3 +1,19 @@
 class Stack
+	attr_accessor :stack
+	def initialize
+		@stack = []
+	end
+
+	def push(var)
+		@stack.push(var)
+	end
+
+	def pop
+		@stack.pop
+	end
+
+	def size
+		@stack.size
+	end
 
 end
